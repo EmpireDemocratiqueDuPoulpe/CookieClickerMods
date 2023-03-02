4363e7319d2a5578048345733060cd25253294e2
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2023-03-02 15:17:09 +0100

    Initial commit

---
+++ LuckyCookieStat/main.js
@@ -0,0 +1,68 @@
+// Object instantiation
+if(LuckyCookieStat === undefined) var LuckyCookieStat = {};
+LuckyCookieStat.id = "LuckyCookieStat";
+LuckyCookieStat.name = "Lucky Cookie Stat";
+LuckyCookieStat.version = "1.0";
+LuckyCookieStat.gameVersion = "2.048";
+LuckyCookieStat.loaded = false;
+
+// Mod definition
+LuckyCookieStat.launch = function () {
+	LuckyCookieStat.init = function () {
+		// Set the mod CSS
+		LuckyCookieStat.styles = ``;
+		CCSE.AddStyles(LuckyCookieStat.styles);
+
+		// Create the HTML components
+		const stats = document.createElement("div");
+		stats.setAttribute("id", "lucky-cookie-stat-box");
+
+		const statsText = document.createElement("span");
+		statsText.setAttribute("id", "lucky-cookie-stat-text");
+
+		stats.append(statsText);
+		l("sectionLeft").append(stats);
+
+		// Initialize internal values
+		LuckyCookieStat.computedValue = -1;
+		LuckyCookieStat.optimalRatio = 6000;
+
+		// Set the update function
+		LuckyCookieStat.update = function () {
+			const wallet = Game.cookies;
+			const CpS = Game.cookiesPs;
+
+			const ratio = wallet / CpS;
+			if (ratio !== LuckyCookieStat.computedValue) {
+				LuckyCookieStat.computedValue = ratio;
+				l("lucky-cookie-stat-text").innerHTML = `x${Math.round(ratio)}`;
+				l("lucky-cookie-stat-text").style.color = (LuckyCookieStat.optimalRatio <= LuckyCookieStat.computedValue) ? "green" : "white";
+			}
+		};
+
+		// Hooks
+		Game.registerHook("logic", LuckyCookieStat.update);
+
+		// End of initialization
+		LuckyCookieStat.loaded = true;
+		if (Game.prefs.popups) Game.Popup(`${LuckyCookieStat.name} loaded!`);
+		else Game.Notify(`${LuckyCookieStat.name} loaded!`, "", "", 1, 1);
+	};
+
+	// Register the mod
+	if (CCSE.ConfirmGameVersion(LuckyCookieStat.name, LuckyCookieStat.version, LuckyCookieStat.gameVersion)) {
+		Game.registerMod(LuckyCookieStat.id, LuckyCookieStat);
+	}
+};
+
+
+if (!LuckyCookieStat.loaded) {
+	if (CCSE && CCSE.isLoaded) {
+		LuckyCookieStat.launch();
+	} else {
+		if (!CCSE) var CCSE = {};
+		if (!CCSE.postLoadHooks) CCSE.postLoadHooks = [];
+
+		CCSE.postLoadHooks.push(LuckyCookieStat.launch);
+	}
+}
